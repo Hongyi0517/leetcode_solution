6b52dc46f8b5d487ce7cfad38c594c24f20b0a6e
Hongyi0517 <a0979260517@icloud.com>
2025-08-09 23:48:12 +0800

    Add Two Sum solution and 2025-08-09 note

---
+++ Two_Sum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution{
+    public:
+        vector<int> twoSum (vector<int>& nums, int target) {
+            for (int i = 0; i < nums.size(); i++) {
+                for(int j = i+1; j < nums.size(); j++){
+                    if (nums[i] + nums[j] == target) {
+                        return {i,j};
+                    }
+                }
+            }
+            return{};
+        }
+};
+
+int main(){
+    Solution solution;
+    vector<int> nums = {2,5,18,64,98};
+    int target = 82;
+    vector<int> result = solution.twoSum(nums,target);
+    
+    if(result.empty()){
+        cout << "No answer find." << endl;
+    }else{
+        cout << "answer:[" << result[0] << "," << result[1] <<"]" << endl;
+    }
+    
+    cin.get();
+    return 0;
+}
